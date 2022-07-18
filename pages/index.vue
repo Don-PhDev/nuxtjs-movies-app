feb323ab1c5059a955cadbbec11c24f55329e081
Don Forrest <forrest@philippinedev.com>
2022-07-18 22:22:24 +0800

    Add styles for search input

--- pages/index.vue
+++ pages/index.vue
@@ -80,6 +80,25 @@ export default {
     padding-top: 120px;
     align-items: flex-start;
   }
+  .search {
+    display: flex;
+    padding: 32px 16px;
+    input {
+      max-width: 350px;
+      width: 100%;
+      padding: 12px 6px;
+      font-size: 14px;
+      border: none;
+      &:focus {
+        outline: none;
+      }
+    }
+    .button {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+  }
+
   .movies {
     padding: 32px 16px;
     .movies-grid {
