483402c4a1aa6a94b12318395c6e9fabff20e495
Don Forrest <forrest@philippinedev.com>
2022-07-20 21:50:29 +0800

    Populate individual movie details route with TMDB data

--- pages/movies/_movieid.vue
+++ pages/movies/_movieid.vue
@@ -1,10 +1,73 @@
 <template>
-  <div class="container single-movie">
+  <Loading v-if="$fetchState.pending" />
+  <div v-else class="container single-movie">
+    <NuxtLink class="button" :to="{name: 'index'}">Back</NuxtLink>
+    <div class="movie-info">
+      <div class="movie-img">
+        <img
+          :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+          alt=""
+        />
+      </div>
+      <div class="movie-content">
+        <h1>Title: {{ movie.title }}</h1>
+        <p class="movie-fact tagline">
+          <span>Tagline:</span> "{{ movie.tagline }}"
+        </p>
+        <p class="movie-fact">
+          <span>Released:</span>
+          {{
+            new Date(movie.release_date).toLocaleString("en-us", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })
+          }}
+        </p>
+        <p class="movie-fact">
+          <span>Duration:</span> {{ movie.runtime }} minutes
+        </p>
+        <p v-if="movie.revenue !== 0" class="movie-fact">
+          <span>Revenue:</span>
+          {{
+            movie.revenue.toLocaleString("en-us", {
+              style: "currency",
+              currency: "USD",
+            })
+          }}
+        </p>
+        <p class="movie-fact">
+          <span>Overview:</span>
+          {{ movie.overview }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
+  name: "SingleMovie",
+  data() {
+    return {
+      movie: null,
+    }
+  },
+  async fetch() {
+    await this.getSingleMovie()
+  },
+  fetchDelay: 1000,
+  methods: {
+    async getSingleMovie() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/${this.$route.params.movieid}?api_key=589a0bba7dc58714016e2ba181e8e1f5&language=en-US`
+      )
+      const result = await data
+      this.movie = result.data
+    }
+  }
 }
 </script>
 
